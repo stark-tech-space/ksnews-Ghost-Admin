ccbacfc4ded003eafed9e30b9dc6047276357a19
Louis Lin <91217075+linlolo@users.noreply.github.com>
2022-07-29 17:40:46 +0800

    translate member filter. (missing some filter values)

--- app/components/modal-delete-tag.js
+++ app/components/modal-delete-tag.js
@@ -1,32 +1,34 @@
 import ModalComponent from 'ghost-admin/components/modal-base';
-import {alias} from '@ember/object/computed';
-import {computed} from '@ember/object';
-import {task} from 'ember-concurrency';
-import {inject as service} from '@ember/service';
+import { alias } from '@ember/object/computed';
+import { computed } from '@ember/object';
+import { task } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
 
 export default ModalComponent.extend({
-  intl: service();
+  intl: service(),
 
-    // Allowed actions
-    confirm: () => {},
+  // Allowed actions
+  confirm: () => {},
 
-    tag: alias('model'),
+  tag: alias('model'),
 
-    postInflection: computed('tag.count.posts', function () {
-        return this.get('tag.count.posts') > 1 ? this.intl.t('Manual.Tags.posts') : this.intl.t('Manual.Tags.post');
-    }),
+  postInflection: computed('tag.count.posts', function () {
+    return this.get('tag.count.posts') > 1
+      ? this.intl.t('Manual.Tags.posts')
+      : this.intl.t('Manual.Tags.post');
+  }),
 
-    actions: {
-        confirm() {
-            this.deleteTag.perform();
-        }
+  actions: {
+    confirm() {
+      this.deleteTag.perform();
     },
+  },
 
-    deleteTag: task(function* () {
-        try {
-            yield this.confirm();
-        } finally {
-            this.send('closeModal');
-        }
-    }).drop()
+  deleteTag: task(function* () {
+    try {
+      yield this.confirm();
+    } finally {
+      this.send('closeModal');
+    }
+  }).drop(),
 });
