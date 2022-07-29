71a7dfe26d7138d8b1d0b357bbe905a0461e593f
Louis Lin <91217075+linlolo@users.noreply.github.com>
2022-07-29 15:07:24 +0800

    translate pages and posts settings

--- app/components/gh-psm-visibility-input.js
+++ app/components/gh-psm-visibility-input.js
@@ -1,39 +1,44 @@
 import Component from '@ember/component';
 import classic from 'ember-classic-decorator';
-import {action, computed} from '@ember/object';
-import {inject as service} from '@ember/service';
+import { action, computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 const VISIBILITIES = [
-    {label: 'Public', name: 'public'},
-    {label: 'Members only', name: 'members'},
-    {label: 'Paid-members only', name: 'paid'}
+  { label: 'Public', name: 'public' },
+  { label: 'Members_only', name: 'members' },
+  { label: 'Paid-members_only', name: 'paid' },
 ];
 
 @classic
 export default class GhPsmVisibilityInput extends Component {
-    @service settings;
-    
-    // public attrs
-    post = null;
+  @service settings;
+  @service intl;
 
-    @computed('post.visibility')
-    get selectedVisibility() {
-        return this.get('post.visibility') || this.settings.get('defaultContentVisibility');
-    }
+  // public attrs
+  post = null;
 
-    init() {
-        super.init(...arguments);
-        this.availableVisibilities = [...VISIBILITIES];
-        this.availableVisibilities.push(
-            {label: 'Specific tier(s)', name: 'tiers'}
-        );
-    }
+  @computed('post.visibility')
+  get selectedVisibility() {
+    return this.get('post.visibility') || this.settings.get('defaultContentVisibility');
+  }
 
-    @action
-    updateVisibility(newVisibility) {
-        this.post.set('visibility', newVisibility);
-        if (newVisibility !== 'tiers') {
-            this.post.set('tiers', []);
-        }
+  init() {
+    super.init(...arguments);
+    this.availableVisibilities = VISIBILITIES.map(({ label, name }) => ({
+      label: this.intl.t(`Manual.Settings.${label}`),
+      name: name,
+    }));
+    this.availableVisibilities.push({
+      label: this.intl.t('Manual.Settings.Specific_tier_s'),
+      name: 'tiers',
+    });
+  }
+
+  @action
+  updateVisibility(newVisibility) {
+    this.post.set('visibility', newVisibility);
+    if (newVisibility !== 'tiers') {
+      this.post.set('tiers', []);
     }
+  }
 }
--- app/components/modals/delete-post.js
+++ app/components/modals/delete-post.js
@@ -1,31 +1,41 @@
 import Component from '@glimmer/component';
-import {inject as service} from '@ember/service';
-import {task} from 'ember-concurrency';
+import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency';
 
 export default class DeletePostModal extends Component {
-    @service notifications;
-    @service router;
+  @service notifications;
+  @service router;
 
-    @task({drop: true})
-    *deletePostTask() {
-        try {
-            const {post} = this.args.data;
+  get messageTitle() {
+    const { post } = this.args.data;
+    console.log(post);
+    console.log(post.displayName);
+    console.log(post.isDeleted);
+    return post.displayName === 'page'
+      ? 'Are_you_sure_you_want_to_delete_this_post?'
+      : 'Are_you_sure_you_want_to_delete_this_page?';
+  }
 
-            if (post.isDeleted) {
-                return true;
-            }
+  @task({ drop: true })
+  *deletePostTask() {
+    try {
+      const { post } = this.args.data;
 
-            // clear the data store and post of any unsaved client-generated tags before deleting
-            post.updateTags();
-            yield post.destroyRecord();
+      if (post.isDeleted) {
+        return true;
+      }
 
-            this.notifications.closeAlerts('post.delete');
-            this.router.transitionTo(post.displayName === 'page' ? 'pages' : 'posts');
-            return true;
-        } catch (error) {
-            this.notifications.showAPIError(error, {key: 'post.delete.failed'});
-        } finally {
-            this.args.close();
-        }
+      // clear the data store and post of any unsaved client-generated tags before deleting
+      post.updateTags();
+      yield post.destroyRecord();
+
+      this.notifications.closeAlerts('post.delete');
+      this.router.transitionTo(post.displayName === 'page' ? 'pages' : 'posts');
+      return true;
+    } catch (error) {
+      this.notifications.showAPIError(error, { key: 'post.delete.failed' });
+    } finally {
+      this.args.close();
     }
+  }
 }
