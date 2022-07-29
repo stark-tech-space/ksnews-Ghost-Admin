16504deda31cac15c6031ba6da60d7e7d251ea17
Louis Lin <91217075+linlolo@users.noreply.github.com>
2022-07-29 16:21:19 +0800

    translate tags page

--- app/components/modal-delete-tag.js
+++ app/components/modal-delete-tag.js
@@ -2,15 +2,18 @@ import ModalComponent from 'ghost-admin/components/modal-base';
 import {alias} from '@ember/object/computed';
 import {computed} from '@ember/object';
 import {task} from 'ember-concurrency';
+import {inject as service} from '@ember/service';
 
 export default ModalComponent.extend({
+  intl: service();
+
     // Allowed actions
     confirm: () => {},
 
     tag: alias('model'),
 
     postInflection: computed('tag.count.posts', function () {
-        return this.get('tag.count.posts') > 1 ? 'posts' : 'post';
+        return this.get('tag.count.posts') > 1 ? this.intl.t('Manual.Tags.posts') : this.intl.t('Manual.Tags.post');
     }),
 
     actions: {
