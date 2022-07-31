0922cf81078b74852bdb87b048c742ce2bb0c7f4
Louis Lin <91217075+linlolo@users.noreply.github.com>
2022-07-31 17:37:56 +0800

    translate dashboard

--- app/components/dashboard/charts/engagement.js
+++ app/components/dashboard/charts/engagement.js
@@ -1,118 +1,128 @@
 import Component from '@glimmer/component';
-import {action} from '@ember/object';
-import {formatNumber} from '../../../helpers/format-number';
-import {inject as service} from '@ember/service';
-import {tracked} from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { formatNumber } from '../../../helpers/format-number';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
-const STATUS_OPTIONS = [{
-    name: 'All members',
-    value: 'total'
-}, {
-    name: 'Paid members',
-    value: 'paid'
-}, {
-    name: 'Free members',
-    value: 'free'
-}];
+const STATUS_OPTIONS = [
+  {
+    name: 'All_members',
+    value: 'total',
+  },
+  {
+    name: 'Paid_members',
+    value: 'paid',
+  },
+  {
+    name: 'Free_members',
+    value: 'free',
+  },
+];
 
 export default class Engagement extends Component {
-    @service dashboardStats;
+  @service dashboardStats;
+  @service intl;
 
-    @action
-    loadCharts() {
-        this.dashboardStats.lastSeenFilterStatus = this.status;
-        this.dashboardStats.loadLastSeen();
-        this.dashboardStats.loadMemberCountStats();
-        this.dashboardStats.loadNewsletterSubscribers();
+  @action
+  loadCharts() {
+    this.dashboardStats.lastSeenFilterStatus = this.status;
+    this.dashboardStats.loadLastSeen();
+    this.dashboardStats.loadMemberCountStats();
+    this.dashboardStats.loadNewsletterSubscribers();
+  }
+
+  @tracked status = 'total';
+  statusOptions = STATUS_OPTIONS.map((statusOption) => ({
+    ...statusOption,
+    name: this.intl.t(`Manual.Dashboard.${statusOption.name}`),
+  }));
+
+  get selectedStatusOption() {
+    return this.statusOptions.find((option) => option.value === this.status);
+  }
+
+  @action
+  onSwitchStatus(selected) {
+    this.status = selected.value;
+    this.dashboardStats.lastSeenFilterStatus = this.status;
+    this.dashboardStats.loadLastSeen();
+  }
+
+  get loading() {
+    return (
+      this.dashboardStats.memberCounts === null ||
+      !this.dashboardStats.memberCounts[this.status] ||
+      this.dashboardStats.membersLastSeen30d === null ||
+      this.dashboardStats.membersLastSeen7d === null
+    );
+  }
+
+  get data30Days() {
+    // fake empty data
+    if (this.isTotalMembersZero) {
+      return '30%';
     }
 
-    @tracked status = 'total';
-    statusOptions = STATUS_OPTIONS;
+    if (this.loading) {
+      return '- %';
+    }
+    const total = this.dashboardStats.memberCounts[this.status];
+    const part = this.dashboardStats.membersLastSeen30d;
 
-    get selectedStatusOption() {
-        return this.statusOptions.find(option => option.value === this.status);
+    if (total <= 0) {
+      return '- %';
     }
 
-    @action 
-    onSwitchStatus(selected) {
-        this.status = selected.value;
-        this.dashboardStats.lastSeenFilterStatus = this.status;
-        this.dashboardStats.loadLastSeen();
+    const percentage = Math.round((part / total) * 100);
+    return `${percentage}%`;
+  }
+
+  get data7Days() {
+    // fake empty data
+    if (this.isTotalMembersZero) {
+      return '60%';
     }
 
-    get loading() {
-        return this.dashboardStats.memberCounts === null
-            || !this.dashboardStats.memberCounts[this.status]
-            || this.dashboardStats.membersLastSeen30d === null 
-            || this.dashboardStats.membersLastSeen7d === null;
+    if (this.loading) {
+      return '- %';
     }
-    
-    get data30Days() {
-        // fake empty data
-        if (this.isTotalMembersZero) {
-            return '30%';
-        }
+    const total = this.dashboardStats.memberCounts[this.status];
+    const part = this.dashboardStats.membersLastSeen7d;
 
-        if (this.loading) {
-            return '- %';
-        }
-        const total = this.dashboardStats.memberCounts[this.status];
-        const part = this.dashboardStats.membersLastSeen30d;
-        
-        if (total <= 0) {
-            return '- %';
-        }
-
-        const percentage = Math.round(part / total * 100);
-        return `${percentage}%`;
+    if (total <= 0) {
+      return '- %';
     }
 
-    get data7Days() {
-        // fake empty data
-        if (this.isTotalMembersZero) {
-            return '60%';
-        }
+    const percentage = Math.round((part / total) * 100);
+    return `${percentage}%`;
+  }
 
-        if (this.loading) {
-            return '- %';
-        }
-        const total = this.dashboardStats.memberCounts[this.status];
-        const part = this.dashboardStats.membersLastSeen7d;
-        
-        if (total <= 0) {
-            return '- %';
-        }
-
-        const percentage = Math.round(part / total * 100);
-        return `${percentage}%`;
+  get dataSubscribers() {
+    // fake empty data
+    if (this.isTotalMembersZero) {
+      return '123';
     }
 
-    get dataSubscribers() {
-        // fake empty data
-        if (this.isTotalMembersZero) {
-            return '123';
-        }
-
-        if (!this.dashboardStats.newsletterSubscribers) {
-            return '-';
-        }
-
-        return formatNumber(this.dashboardStats.newsletterSubscribers[this.status]);
+    if (!this.dashboardStats.newsletterSubscribers) {
+      return '-';
     }
 
-    get dataEmailsSent() {
-        return this.dashboardStats.emailsSent30d ?? 0;
-    }
+    return formatNumber(this.dashboardStats.newsletterSubscribers[this.status]);
+  }
 
-    get hasPaidTiers() {
-        return this.dashboardStats.siteStatus?.hasPaidTiers;
-    }
+  get dataEmailsSent() {
+    return this.dashboardStats.emailsSent30d ?? 0;
+  }
 
-    get totalMembers() {
-        return this.dashboardStats.memberCounts?.total ?? 0;
-    }
+  get hasPaidTiers() {
+    return this.dashboardStats.siteStatus?.hasPaidTiers;
+  }
 
-    get isTotalMembersZero() {
-        return this.dashboardStats.memberCounts && this.totalMembers === 0;
-    }
+  get totalMembers() {
+    return this.dashboardStats.memberCounts?.total ?? 0;
+  }
+
+  get isTotalMembersZero() {
+    return this.dashboardStats.memberCounts && this.totalMembers === 0;
+  }
 }
